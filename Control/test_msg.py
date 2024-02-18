6bb16921000e6cea17909889d816e2ffb27eda72
0xinsanity <noahsahar@gmail.com>
2024-02-17 17:59:34 -0800

    update to djitellopy

--- Control/test_msg.py
+++ Control/test_msg.py
@@ -16,6 +16,26 @@ import djitellopy
 
 from helper_functions import *
 
+import math
+
+position_drone_1 = np.zeros((1, 3), dtype=np.float32)
+orientation_drone_1 = np.zeros((1, 4), dtype=np.float32)
+
+goal_position = np.zeros((1, 3), dtype=np.float32)
+
+
+def euler_to_quaternion(roll, pitch, yaw):
+    qx = math.sin(roll / 2) * math.cos(pitch / 2) * math.cos(
+        yaw / 2) - math.cos(roll / 2) * math.sin(pitch / 2) * math.sin(yaw / 2)
+    qy = math.cos(roll / 2) * math.sin(pitch / 2) * math.cos(
+        yaw / 2) + math.sin(roll / 2) * math.cos(pitch / 2) * math.sin(yaw / 2)
+    qz = math.cos(roll / 2) * math.cos(pitch / 2) * math.sin(
+        yaw / 2) - math.sin(roll / 2) * math.sin(pitch / 2) * math.cos(yaw / 2)
+    qw = math.cos(roll / 2) * math.cos(pitch / 2) * math.cos(
+        yaw / 2) + math.sin(roll / 2) * math.sin(pitch / 2) * math.sin(yaw / 2)
+
+    return [qw, qx, qy, qz]
+
 
 def main():
     global obs_1_data, recurrent_data
@@ -35,7 +55,24 @@ def main():
 
         # skip first 300 frames
         while True:
-            print(drone.query_attitude())
+            global position_drone_1, orientation_drone_1
+
+            msg_orientation = drone.query_attitude()
+            print(msg_orientation)
+            position_drone_1[0, 0] = 0
+            position_drone_1[0, 1] = 0
+            position_drone_1[0, 2] = 0
+
+            [qw, qx, qy, qz] = euler_to_quaternion(msg_orientation["roll"],
+                                                   msg_orientation["pitch"],
+                                                   msg_orientation["yaw"])
+            orientation_drone_1[0, 0] = qx
+            orientation_drone_1[0, 1] = qy
+            orientation_drone_1[0, 2] = qz
+            orientation_drone_1[0, 3] = qw
+
+            print(orientation_drone_1)
+
             time.sleep(2)
     except Exception as ex:
         exc_type, exc_value, exc_traceback = sys.exc_info()
